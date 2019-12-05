806cc1dd362fc7b16abbc750e98e51abcb149567
Lael <laelav@gmail.com>
2019-12-05 16:58:23 +0200

    graphql commit 2

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -3,26 +3,31 @@ module.exports = function (app, db) {
 
     //sending 2 nums to DB
     app.post('/sendnums/:num1/:num2', (req, res) => {
-        let temp1 = Number(req.params.num1);
-        let temp2 = Number(req.params.num2);
-        const note = {
-            num1: temp1,
-            num2: temp2,
-            addition: (temp1+temp2).toString(),
-            multiply: (temp1*temp2).toString()
-        };
-        console.log(note);
+        if (!Number(req.params.num1) || !Number(req.params.num2)){
+            console.log("You must enter numbers!");
+            res.send({'message': "You must enter numbers!"});
+        }else{
+            let temp1 = Number(req.params.num1);
+            let temp2 = Number(req.params.num2);
+            const note = {
+                num1: req.params.num1,
+                num2: req.params.num2,
+                addition: (temp1+temp2).toString(),
+                multiply: (temp1*temp2).toString()
+            };
+            console.log(note);
 
-        db.collection('Table').insertOne(note, (err, result) => {
-            if (err) {
-                res.send({'error': 'An error has occurred in Post /register.'});
-            } else {
-                //Send back to front.
-                console.log("Numbers added successfully");
-                res.send({'message': "Numbers added successfully"});
-            }
-        });
+            db.collection('Table').insertOne(note, (err, result) => {
+                if (err) {
+                    res.send({'error': 'An error has occurred in Post /register.'});
+                } else {
+                    //Send back to front.
+                    console.log("Numbers added successfully");
+                    res.send({'message': "Numbers added successfully"});
+                }
+            });
 
+        }
     });
 
 };
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const graphqlHTTP = require("express-graphql");
 //const graphqlSchema = require('./graphql/schema');
 //const graphqlResolver = require('./graphql/resolvers');
 const { buildSchema } = require("graphql");
+const mongoose = require('mongoose');
+const Event = require('./models/event');
 
 var db = require("./config/db");
 var app = express();
@@ -12,13 +14,36 @@ var app = express();
 const port = 8000;
 app.use(bodyParser.json());
 
+app.use((req,res,next)=>{
+   // console.log(res.status());
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type');
+    if (req.method === 'OPTIONS'){
+        return res.sendStatus(200);
+    }
+    next();
+});
+
+
 
 app.use('/graphql', graphqlHTTP({
-    schema: buildSchema('type RootQuery{' +
-        'events: [String!]!' +
+    schema: buildSchema('type Event{' +
+        '_id: ID!,' +
+        'num1: String!,' +
+        'num2: String!,' +
+        'addition: String!,' +
+        'multiply: String!' +
+        '},' +
+        'input EventInput{' +
+        'num1: String!,' +
+        'num2: String!' +
+        '},' +
+        'type RootQuery{' +
+        'events: [Event!]!' +
         '},' +
         'type RootMutation{' +
-        'createEvent(name: String): String' +
+        'createEvent(eventInput:EventInput): Event' +
         '},' +
         'schema{' +
         'query: RootQuery,' +
@@ -26,11 +51,38 @@ app.use('/graphql', graphqlHTTP({
         '}'),
     rootValue: {
         events: () =>{
-            return ['Hello1','Hello2','Hello3'];
+            return Event.find()
+                .then(events =>{
+                    return events.map(event => {
+                        return {...event._doc, _id: event.id};
+                    })
+                })
+                .catch(err => {
+                    throw err;
+                })
         },
         createEvent: (args)=>{
-            const eventName = args.name;
-            return eventName;
+            let tempnum1 = args.eventInput.num1;
+            let tempnum2 = args.eventInput.num2;
+            let tempaddition = (Number(tempnum1)+Number(tempnum2)).toString();
+            let tempmultiply = (+tempnum1*(+tempnum2)).toString();
+            const event = new Event({
+                num1:tempnum1,
+                num2:tempnum2,
+                addition:tempaddition,
+                multiply:tempmultiply
+            })
+            console.log(event);
+            return event
+                .save()
+                .then( result =>{
+                    console.log(result);
+                    return event;
+                }).catch(err =>{
+                    console.log(err);
+                    throw err;
+                });
+            //return event;
         }
     },
     graphiql: true
@@ -38,21 +90,22 @@ app.use('/graphql', graphqlHTTP({
 
 
 MongoClient.connect(
-  db.url,
-  { useUnifiedTopology: true },
-  { useNewUrlParser: true },
-  (err, database) => {
-    if (err) return console.log(err);
+    db.url,
+    { useUnifiedTopology: true },
+    { useNewUrlParser: true },
+    (err, database) => {
+        if (err) return console.log(err);
 
-    db = database.db("reactassignmentserver");
-    require("./app/routes")(app, db);
-
-    app.listen(process.env.PORT || port, () => {
-      console.log(
-        "Express server listening on port %d in %s mode - We Are lIVE!",
-        port,
-        app.settings.env
-      );
-    });
-  }
+        db = database.db("reactassignmentserver");
+        require("./app/routes")(app, db);
+        mongoose.connect("mongodb+srv://laelav:laelav1@reactassignment-lmlti.mongodb.net/numbers?retryWrites=true&w=majority\n");
+        app.listen(process.env.PORT || port, () => {
+            console.log(
+                "Express server listening on port %d in %s mode - We Are lIVE!",
+                port,
+                app.settings.env
+            );
+        });
+    }
 );
+
